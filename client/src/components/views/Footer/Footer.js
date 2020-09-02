8d9a8f1edb5a23401c11b62ed4bf3489c18b9f63
anxiubin <tnqls0120@naver.com>
2020-09-02 14:11:20 +0900

    ✨ add boilerplate : login, logout and signup feature

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Footer() {
+	return (
+		<div
+			style={{
+				height: "80px",
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				justifyContent: "center",
+				fontSize: "0.5rem",
+			}}
+		>
+			<p>made by anxiubin</p>
+		</div>
+	)
+}
+
+export default Footer
